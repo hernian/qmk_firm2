8191d79682bd666a8752a692eb42cf8387ef5389
hernian <hernian@gmail.com>
2025-02-25 17:46:28 +0900

    ファーム更新中のLEDがまぶしいので消した

--- keyboards/hernian/danopad01/config.h
+++ keyboards/hernian/danopad01/config.h
@@ -7,4 +7,3 @@
 
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U // Timeout window in ms in which the double tap can occur.
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17 // Specify a optional status led by GPIO number which blinks when entering the bootloader
